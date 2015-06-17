da14cf0c88b99fb356c64521cbbb2532639d974f
Magnus Skog <magnus.m.skog@gmail.com>
2015-06-17 22:10:19 +0200

    One more

--- app/serializers/album_image_serializer.rb
+++ app/serializers/album_image_serializer.rb
@@ -2,7 +2,7 @@ class AlbumImageSerializer < ActiveModel::Serializer
   attributes :id, :image_url, :image_height, :image_width, :image_id
 
   def image_url
-    url_for(controller: '/images', action: 'show', id: object.image.hash_filename)
+    raw_image_url(object.image.hash_filename)
   end
 
   def image_id
