1d0e7d1ad315547aa71673d76c6c8c9dfd0287d1
Magnus Skog <magnus.m.skog@gmail.com>
2015-07-07 20:05:02 +0200

    Added album_id to album image serializer

--- app/serializers/album_image_serializer.rb
+++ app/serializers/album_image_serializer.rb
@@ -1,5 +1,5 @@
 class AlbumImageSerializer < ActiveModel::Serializer
-  attributes :id, :image_url, :image_height, :image_width, :image_id, :image_file_content_type
+  attributes :id, :image_url, :image_height, :image_width, :image_id, :image_file_content_type, :album_id
 
   def image_url
     raw_image_url(object.image.hash_filename)
