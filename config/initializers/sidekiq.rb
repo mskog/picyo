9767295c52d6efcae5453ac6a21eb56f1e51f1a1
Magnus Skog <magnus.m.skog@gmail.com>
2015-11-19 18:22:23 +0100

    Removed faulty middleware

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,3 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL']}
-
-  Sidekiq::Middleware::Server::RetryJobs.send(:remove_const, :DEFAULT_MAX_RETRY_ATTEMPTS)
-  Sidekiq::Middleware::Server::RetryJobs::DEFAULT_MAX_RETRY_ATTEMPTS = 5
 end
